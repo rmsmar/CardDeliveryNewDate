fbf87e22e7cfadef6c0f7d6fc9413781c54de152
rmsmar <rms.marina89@gmail.com>
2021-11-28 13:24:19 +0300

    Removed folder from repository

---
+++ src/test/java/ru/netology/CardDeliveryTest.java
@@ -0,0 +1,92 @@
+package ru.netology;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class CardDeliveryTest {
+    DataGenerator dataGenerator = new DataGenerator();
+
+    @BeforeEach
+    void Setup() {
+        open("http://localhost:9999");
+    }
+
+    @Test
+    void shouldSubmitRequest() {
+        String name = dataGenerator.makeName();
+        String phone = dataGenerator.makePhone();
+        String city = dataGenerator.makeCity();
+
+        $("[placeholder='Город']").setValue(city);
+        $("[placeholder='Дата встречи']").doubleClick().sendKeys(dataGenerator.forwardDate(3));
+        $("[name=name]").setValue(name);
+        $("[name=phone]").setValue(phone);
+        $(".checkbox__box").click();
+        $(".button__text").click();
+        $(withText("Успешно")).shouldBe(visible);
+        $("input[placeholder=\"Дата встречи\"]").doubleClick().sendKeys(Keys.BACK_SPACE);
+        $("[placeholder='Дата встречи']").doubleClick().sendKeys(dataGenerator.forwardDate(4));
+        $(".button__text").click();
+        $(withText("У вас уже запланирована встреча на другую дату. Перепланировать?")).shouldBe(visible);
+        $("[data-test-id=replan-notification] button.button").click();
+        $(withText("Успешно")).shouldBe(visible);
+    }
+
+    @Test
+    void shoulNotSubmitWithoutCity() {
+        $("[placeholder='Дата встречи']").doubleClick().sendKeys(dataGenerator.forwardDate(3));
+        $("[name=name]").setValue(dataGenerator.makeName());
+        $("[name=phone]").setValue(dataGenerator.makePhone());
+        $(".checkbox__box").click();
+        $(".button__text").click();
+        $(".input_theme_alfa-on-white.input_invalid .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
+    }
+
+    @Test
+    void shouldNotSubmitWithoutName() {
+        $("[placeholder='Город']").setValue(dataGenerator.makeCity());
+        $("[placeholder='Дата встречи']").doubleClick().sendKeys(dataGenerator.forwardDate(3));
+        $("[name=phone]").setValue(dataGenerator.makePhone());
+        $(".checkbox__box").click();
+        $(".button__text").click();
+        $(".input_theme_alfa-on-white.input_invalid .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
+    }
+
+    @Test
+    void shouldNotSumbitWithIncorrectName() {
+        $("[placeholder='Город']").setValue(dataGenerator.makeCity());
+        $("[placeholder='Дата встречи']").doubleClick().sendKeys(dataGenerator.forwardDate(3));
+        $("[name=name]").setValue("Vasiliy Ivanov");
+        $("[name=phone]").setValue(dataGenerator.makePhone());
+        $(".checkbox__box").click();
+        $(".button__text").click();
+        $(".input_theme_alfa-on-white.input_invalid .input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+    }
+
+    @Test
+    void shouldNotSubmitWithoutPhone() {
+        $("[placeholder='Город']").setValue(dataGenerator.makeCity());
+        $("[placeholder='Дата встречи']").doubleClick().sendKeys(dataGenerator.forwardDate(3));
+        $("[name=name]").setValue(dataGenerator.makeName());
+        $(".checkbox__box").click();
+        $(".button__text").click();
+        $(".input_theme_alfa-on-white.input_invalid .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
+    }
+
+    @Test
+    void shouldNotSubmitWithoutCheckbox() {
+        $("[placeholder='Город']").setValue(dataGenerator.makeCity());
+        $("[placeholder='Дата встречи']").doubleClick().sendKeys(dataGenerator.forwardDate(3));
+        $("[name=name]").setValue(dataGenerator.makeName());
+        $("[name=phone]").setValue(dataGenerator.makePhone());
+        $(".button__text").click();
+        $(".input_invalid").shouldHave(exactText("Я соглашаюсь с условиями обработки и использования моих персональных данных"));
+    }
+}
--- src/test/java/ru/netology/DataGenerator.java
+++ src/test/java/ru/netology/DataGenerator.java
@@ -3,28 +3,32 @@ package ru.netology;
 import com.github.javafaker.Faker;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 
 public class DataGenerator {
-    private DataGenerator() {
+    private Faker faker;
+    private LocalDate today = LocalDate.now();
+    private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+
+    public String forwardDate(int plusDays) {
+        LocalDate newDate = today.plusDays(plusDays);
+        return formatter.format(newDate);
     }
 
-    public static class Registration {
-        private Registration() {
-        }
+    public String makeCity() {
+        String[] myCityList = new String[]{"Абакан", "Анадырь", "Архангельск", "Астрахань", "Барнаул", "Владикавказ", "Горно-Алтайск", "Йошкар-Ола", "Казань", "Калининград", "Калуга", "Краснодар", "Магас", "Махачкала", "Нарьян-Мар", "Салехард", "Самара", "Саранск", "Саратов", "Хабаровск", "Ханты-Мансийск", "Южно-Сахалинск", "Великий Новгород", "Владивосток", "Владимир", "Вологда", "Рязань", "Биробиджан", "Чебоксары", "Москва", "Санкт-Петербург", "Ульяновск", "Симферополь", "Ростов-на-Дону"};
+        int city = (int) Math.floor(Math.random() * myCityList.length);
+        return myCityList[city];
+    }
 
-        public static Meeting generate() {
-            Faker faker = new Faker(new Locale("ru"));
-            LocalDate futureDateFirstMeeting = LocalDate.now().plusDays(5);
-            LocalDate futureDateSecondMeeting = LocalDate.now().plusDays(7);
+    public String makeName() {
+        faker = new Faker(new Locale("ru"));
+        return faker.name().fullName();
+    }
 
-            return new Meeting(
-                    faker.address().city(),
-                    futureDateFirstMeeting,
-                    faker.name().firstName(),
-                    faker.name().lastName(),
-                    faker.phoneNumber().cellPhone(),
-                    futureDateSecondMeeting);
-        }
+    public String makePhone() {
+        faker = new Faker(new Locale("ru"));
+        return faker.phoneNumber().phoneNumber();
     }
 }
